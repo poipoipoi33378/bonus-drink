6382d7b2aadc40cba307890ff1b4f407ff36fce9
菅原誠一 <poipoipoi33378@gmail.com>
2018-05-02 22:01:24 +0900

    total_count_for がぐちゃぐちゃしていたので、再帰表現で書きかえ

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,15 +1,18 @@
 class BonusDrink
+  BONUS_COUNT = 3
+
   def self.total_count_for(amount)
+    amount + bonus_count(amount)
+  end
 
-    x = amount
-    totalBonus = 0
-
-    #購入分からのボーナス
-    while x >= 3 do
-      bonus = x/3
-      x = bonus+x%3
-      totalBonus = bonus + totalBonus
+  private
+  def self.bonus_count(amount)
+    bonus = amount/BONUS_COUNT
+    nextAmount = bonus + amount%BONUS_COUNT
+    if nextAmount >= 3
+      bonus + bonus_count(nextAmount)
+    else
+      bonus
     end
-    totalBonus + amount
   end
 end
